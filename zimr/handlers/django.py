0994db1bfbab2e5720e2619abd1bc3438d875e45
Ian Halpern <ian@ian-halpern.com>
2011-04-08 21:10:41 -0400

    * fixed django bug with SCRIPT_NAME

--- zimr/handlers/django.py
+++ zimr/handlers/django.py
@@ -4,12 +4,18 @@ from functools import partial
 import zimr.handlers.wsgi
 
 # reference /usr/lib/pymodules/python2.6/django/core/management/commands/runserver.py
-
-os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
+if 'DJANGO_SETTINGS_MODULE' not in os.environ:
+	os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
 
 import django.core.handlers.wsgi
 from django.core.servers.basehttp import AdminMediaHandler, WSGIServerException
 
-application = AdminMediaHandler( django.core.handlers.wsgi.WSGIHandler(), '' )
+_application = AdminMediaHandler( django.core.handlers.wsgi.WSGIHandler(), '' )
+
+def application( environ, start_response ):
+	# django SCRIPT_NAME fix
+	environ['PATH_INFO'] = environ['SCRIPT_NAME'] + environ['PATH_INFO']
+	environ['SCRIPT_NAME'] = ''
+	return _application( environ, start_response )
 
 connection_handler = partial( zimr.handlers.wsgi.connection_handler, application )
